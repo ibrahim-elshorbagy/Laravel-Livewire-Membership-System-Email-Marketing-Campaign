7dcedbcbd95f33d85434e400bcd57e348d226cc9
ibrahim-elshorbagy <e.mohmed55@gmail.com>
2025-03-09 23:54:43 +0200

    solve page expire problem

--- app/Livewire/Pages/Auth/Logout.php
+++ app/Livewire/Pages/Auth/Logout.php
@@ -10,10 +10,20 @@ class Logout extends Component
 
     public function logout()
     {
+        if (session()->has('impersonated_by')) {
+            session()->forget('impersonated_by');
+        }
+
         Auth::logout();
+
+        // Regenerate session after logout
+        session()->invalidate();
+        session()->regenerateToken();
+
         return $this->redirect('/', navigate: true);
 
     }
+
     public function render()
     {
         return view('livewire.pages.auth.logout');
