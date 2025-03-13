e291eb041f87a8e737e351a7b6c36a4598ff9668
ibrahim-elshorbagy <e.mohmed55@gmail.com>
2025-03-13 22:02:26 +0200

    x-cloak

--- resources/views/livewire/pages/user/emails/campaign/campaign-list.blade.php
+++ resources/views/livewire/pages/user/emails/campaign/campaign-list.blade.php
@@ -144,7 +144,7 @@
                                     </button>
                                     @if(!$campaign->canBeActive())
                                     <!-- Tooltip -->
-                                    <div x-show="showTooltip" x-transition:enter="transition ease-out duration-200"
+                                    <div x-show="showTooltip" x-cloak x-transition:enter="transition ease-out duration-200"
                                         x-transition:enter-start="opacity-0 translate-y-1"
                                         x-transition:enter-end="opacity-100 translate-y-0"
                                         x-transition:leave="transition ease-in duration-150"
